7f1d5a0a2d6b3fea88b556e5d19bb27ec61c0c79
PolaarOfficial <69478179+PolaarOfficial@users.noreply.github.com>
2023-05-27 12:20:01 -0400

    GETIT-12 : added functionality to hit the url api (#9)

--- server/initserver.py
+++ server/initserver.py
@@ -3,6 +3,8 @@ import base64
 import json
 app = Flask(__name__)
 
+urls = {}
+
 @app.route('/digest', methods=['POST'])
 def initDigest():
     file = request.form['imagefile'].split('base64,')[1]
@@ -12,6 +14,19 @@ def initDigest():
 @app.route('/health_check', methods=['GET'])
 def health_check():
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
+@app.route('/url', methods=['GET', 'POST'])
+def urlReader():
+    print(request.json['url'])
+    if request.method == 'GET':
+        if request.json['url'] in urls:
+            return json.dumps({'found':True}), 200, {'ContentType':'application/json'}
+        else:
+            return json.dumps({'found':False}), 200, {'ContentType':'application/json'}
+    elif request.method == 'POST':
+        urls[request.json['url']] = request.json['ecommerce']
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
 
 def base_64_to_image(base64_string):
     imgdata = base64.b64decode(base64_string)
